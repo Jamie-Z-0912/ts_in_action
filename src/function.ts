e0488fa104dae94f6c651fec268597b3aaf2c525
Jamie <>
2020-02-11 14:07:40 +0800

    函数重载

--- src/function.ts
+++ src/function.ts
@@ -21,6 +21,22 @@ console.log(add6(10, undefined, 20));
 function add7(x: number, ...rest: number[]) {
   return x + rest.reduce((pre, cur) => pre + cur);
 }
+console.log(add7(1, 2, 3, 4, 5));
+
+/** 函数重载 **/
+function add8(...rest: number[]): number;
+function add8(...rest: string[]): string;
+function add8(...rest: any[]): any {
+  let first = rest[0];
+  if (typeof first === "string") {
+    return rest.join("");
+  }
+  if (typeof first === "number") {
+    return rest.reduce((pre, cur) => pre + cur);
+  }
+}
+console.log(add8(1, 2, 3));
+console.log(add8("a", "b", "c"));
 
 module.exports = {
   add6
